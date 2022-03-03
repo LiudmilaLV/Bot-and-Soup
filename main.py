d70dd32a0d788e42f6c82de32fd4d64b6a94a729
LiudmilaLV <lisikhina.lv@gmail.com>
2022-03-03 19:25:47 +0800

    searching in terminal

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-# print('What do you search for?')
-# key_word = input('>')
-# print(f'Serching for  "{key_word}"')
-concerts = []
+
+print('What key word are you looking for?')
+key_word = input('>')
+print(f'Searching for  "{key_word}" ...')
+
 
 html_text = requests.get('https://www.irk.ru/afisha/tours/').text
 soup = BeautifulSoup(html_text, 'lxml')
-events = soup.find_all('li', class_ = 'grid-list__item')
+events = soup.find_all('li', class_='grid-list__item')
 for event in events:
     if event.article:
-        event_type = event.find('div', class_ = 'afisha-article__elements').span.text.replace('  ', '')
+        event_type = event.find(
+            'div', class_='afisha-article__elements').span.text
         if 'концерт' in event_type:
-            event_name = event.find('h4', class_ = 'afisha-article__title').a.text.replace('  ', '')
-            if event_name not in concerts:
-                concerts.append(event_name)
-
-for concert in concerts:
-    print(concert)
+            event_link = event.article.header.a['href']
+            event_text = requests.get(f'https://www.irk.ru{event_link}').text
+            if key_word in event_text:
+                print(event.find('h4', class_='afisha-article__title').a.text.strip())
+                print((f'https://www.irk.ru{event_link}'))
