ea034c9864b7e27e295d42ea7a6f316f8c4c6498
LiudmilaLV <lisikhina.lv@gmail.com>
2022-03-03 20:30:33 +0800

    linters added

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import requests
-
 from bs4 import BeautifulSoup
-
 
 print("What key word are you looking for?")
 key_word = input(">")
@@ -13,8 +11,7 @@ soup = BeautifulSoup(html_text, "lxml")
 events = soup.find_all("li", class_="grid-list__item")
 for event in events:
     if event.article:
-        event_type = event.find(
-            "div", class_="afisha-article__elements").span.text
+        event_type = event.find("div", class_="afisha-article__elements").span.text
         if "концерт" in event_type:
             event_link = event.article.header.a["href"]
             event_text = requests.get(f"https://www.irk.ru{event_link}").text
