198912716bd8c663e11ac66837784d7999d8d800
LiudmilaLV <lisikhina.lv@gmail.com>
2022-03-03 19:59:44 +0800

    local linter  checked

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import requests
+
 from bs4 import BeautifulSoup
 
 
-print('What key word are you looking for?')
-key_word = input('>')
+print("What key word are you looking for?")
+key_word = input(">")
 print(f'Searching for  "{key_word}" ...')
 
 
-html_text = requests.get('https://www.irk.ru/afisha/tours/').text
-soup = BeautifulSoup(html_text, 'lxml')
-events = soup.find_all('li', class_='grid-list__item')
+html_text = requests.get("https://www.irk.ru/afisha/tours/").text
+soup = BeautifulSoup(html_text, "lxml")
+events = soup.find_all("li", class_="grid-list__item")
 for event in events:
     if event.article:
         event_type = event.find(
-            'div', class_='afisha-article__elements').span.text
-        if 'концерт' in event_type:
-            event_link = event.article.header.a['href']
-            event_text = requests.get(f'https://www.irk.ru{event_link}').text
+            "div", class_="afisha-article__elements").span.text
+        if "концерт" in event_type:
+            event_link = event.article.header.a["href"]
+            event_text = requests.get(f"https://www.irk.ru{event_link}").text
             if key_word in event_text:
-                print(event.find('h4', class_='afisha-article__title').a.text.strip())
-                print((f'https://www.irk.ru{event_link}'))
+                print(event.find("h4", class_="afisha-article__title").a.text.strip())
+                print((f"https://www.irk.ru{event_link}"))
