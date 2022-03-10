04bf6334910bfc51619af77a12fc000fbdc92282
LiudmilaLV <lisikhina.lv@gmail.com>
2022-03-10 18:49:08 +0800

    informing via bot

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Bot-and-Soup
 Scheduled web-scraping and automated bot messaging
-## [![GitHub Super-Linter](https://github.com/LiudmilaLV/Bot-and-Soup/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+## [![Lint Code](https://github.com/LiudmilaLV/Bot-and-Soup/actions/workflows/linter.yml/badge.svg)](https://github.com/LiudmilaLV/Bot-and-Soup/actions/workflows/linter.yml)
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from telegram.ext import *
 
-print("What key word are you looking for?")
-key_word = input(">")
-print(f'Searching for  "{key_word}" ...')
+from constants import API_KEY
+
+# Prompting a key word
+print("Hi! \nI'm going to scrape an event section of irk.ru looking for your key word.")
+print("What are you looking for?")
+key_word = input(" Key word > ")
+print(f'Searching for event articles mentioning "{key_word}" . . . ')
 
 
+# Scrape an event section of irk.ru looking for all concert articles mentioning the key word
 html_text = requests.get("https://www.irk.ru/afisha/tours/").text
 soup = BeautifulSoup(html_text, "lxml")
 events = soup.find_all("li", class_="grid-list__item")
+found_info = []
 for event in events:
     if event.article:
         event_type = event.find("div", class_="afisha-article__elements").span.text
@@ -16,5 +23,20 @@ for event in events:
             event_link = event.article.header.a["href"]
             event_text = requests.get(f"https://www.irk.ru{event_link}").text
             if key_word in event_text:
-                print(event.find("h4", class_="afisha-article__title").a.text.strip())
-                print((f"https://www.irk.ru{event_link}"))
+                found_info.append(
+                    event.find("h4", class_="afisha-article__title").a.text.strip()
+                    + f":\nhttps://www.irk.ru{event_link}"
+                )
+
+
+# Set up a Telegram bot
+updater = Updater(API_KEY, use_context=True)
+
+
+# Inform me about all the found concerts via the bot
+if found_info:
+    updater.bot.send_message(chat_id="-1001775792397", text=f"Анонсы мероприятий, упоминаюшие {key_word}: \n")
+    for event in found_info:
+        updater.bot.send_message(chat_id="-1001775792397", text=event)
+else:
+    updater.bot.send_message(chat_id="-1001775792397", text=f"Статьи, упоминаюшие {key_word} не найдены.")
